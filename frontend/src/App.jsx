570d71a32aca51978db32e94f5c702e007584b45
anildalar <dollorinfotech@gmail.com>
2025-08-07 10:07:11 +0530

    Dynamically get the Business Cateogry Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 //1 Import areaa
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { BASE_URL } from './lib/Helper';
 import axios from 'axios';
@@ -7,8 +7,24 @@ import Swal from 'sweetalert2'; // ✅ SweetAlert2 import
 
 //2. Function defination area
 function App() {
-  //2.1
+  //2.1 Hooks Area Hooks Variable/Hooks Function
   const [fname,setFname] = useState('');
+  const [businessCategory,setBusinessCategory] = useState([]);
+  //useEffect(<function>, <dependencyArray>)
+  useEffect(()=>{
+    //I am going to call api
+    try {
+        axios.get(`${BASE_URL}/api/business-categories?filters[is_featured][$eq]=true`).then((res)=>{
+           // handle success
+          console.log(res?.data?.data);
+          setBusinessCategory(res?.data?.data);
+        }).catch((err)=>{
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  },[]);
 
   //2.2 Function defination area
   let sendData = async ()=>{
@@ -52,12 +68,25 @@ function App() {
   }
 
   //2.3 Return statemnt
+  //array.map(function(currentValue, index, arr))
+
   return (
    <>
     <h1>Create Operation</h1>
 
       <input type="text" name="fname" placeholder="Enter your name" onChange={handleChange} />
       <input type="button" value="send" onClick={sendData} />
+      <hr />
+      <ul>
+        {
+          businessCategory.map((cv,idx,arr)=>{
+            //Every function may return something
+            return <li><a href="#"><img src={cv.icon_url} />{cv.category_name}</a></li>
+          })
+        }
+        
+      </ul>
+
    </>
   )
 }
